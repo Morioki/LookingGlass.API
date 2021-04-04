88908d374f11b075b42895f296a129f7cd7d29d7
Garrett Braatz <morioki@protonmail.com>
2021-04-04 14:27:51 -0700

    Add placeholder for platform resolvers

--- api/resolvers/platform.py
+++ api/resolvers/platform.py
@@ -1,4 +1,6 @@
+from api.base import db
 from api.models import Platforms
+from api.helpers import NoChangeError
 from ariadne import convert_kwargs_to_snake_case
 
 
@@ -22,3 +24,13 @@ def resolve_platform(obj, info, platform_id):
     except AttributeError: # TODO handle error data
         pass
     return payload
+
+
+# * Mutations
+@convert_kwargs_to_snake_case
+def resolve_insert_platform(obj, info, generation_id, platformcode, description, handheld, active):
+    pass
+
+@convert_kwargs_to_snake_case
+def resolve_update_platform(obj, info, platform_id, generation_id=None, platformcode=None, description=None, handheld=None, active=None):
+    pass
