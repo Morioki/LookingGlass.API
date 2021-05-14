49243613beb7a54741b656461c08ab8e6e09768c
Garrett Braatz <grbraatz@gmail.com>
2021-05-14 00:27:47 -0700

    feat: add cors functionality to flask

--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,13 @@
+import json
 from flask import request, jsonify
+from flask_cors import CORS
 from ariadne import graphql_sync
 from ariadne.constants import PLAYGROUND_HTML
 from api.base import app
 from api.helpers import get_user_context
 from api.graphql import schema
+
+CORS(app)
 
 @app.route('/')
 def hello():
@@ -17,6 +21,9 @@ def graphql_playground():
 
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
+    print(request.headers)
+
+    # data = request.get_json() if request.is_json else json.loads(request.data)
     data = request.get_json()
     success, result = graphql_sync(
         schema,
